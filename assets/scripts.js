747cec0bb1490bdcee2b0631930545624762ef8f
Yarmo Mackenbach <yarmo@qivro.xyz>
2020-06-28 23:12:03 +0200

    Add wkd or hkp to profile links

--- assets/scripts.js
+++ assets/scripts.js
@@ -232,11 +232,11 @@ async function displayProfile(opts) {
     feedback += `</div>`;
     feedback += `<div class="profileDataItem profileDataItem--noLabel">`;
     feedback += `<div class="profileDataItem__label"></div>`;
-    feedback += `<div class="profileDataItem__value"><a href="/verify/${opts.input}">verify signature</a></div>`;
+    feedback += `<div class="profileDataItem__value"><a href="/verify/${opts.mode}/${opts.input.replace(".", "_")}">verify signature</a></div>`;
     feedback += `</div>`;
     feedback += `<div class="profileDataItem profileDataItem--noLabel">`;
     feedback += `<div class="profileDataItem__label"></div>`;
-    feedback += `<div class="profileDataItem__value"><a href="/encrypt/${opts.input}">encrypt message</a></div>`;
+    feedback += `<div class="profileDataItem__value"><a href="/encrypt/${opts.mode}/${opts.input.replace(".", "_")}">encrypt message</a></div>`;
     feedback += `</div>`;
 
     // Display feedback
