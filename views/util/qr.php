facebdc99719bee5b3424639b65d5cafaf94740a
Yarmo Mackenbach <yarmo@qivro.xyz>
2020-07-02 22:48:02 +0200

    Fix typo

--- views/util/qr.php
+++ views/util/qr.php
@@ -3,7 +3,7 @@
 <h1>QR Code</h1>
 <div class="content">
     <form id="form-util-qr" method="post">
-        <h3>Fingeprint</h3>
+        <h3>Fingerprint</h3>
         <input type="text" name="input" id="input" placeholder="Fingerprint" value="<?=$this->escape($input)?>">
         <div id="qrcode"></div>
     </form>
