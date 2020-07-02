fcc1e8e57e14e93ecf881075e0af7914802e3f64
Yarmo Mackenbach <yarmo@qivro.xyz>
2020-07-02 09:23:08 +0200

    Add a secrets template file

---
+++ server/secrets.template.php
@@ -0,0 +1,8 @@
+<?php
+
+// Twitter API bearer code
+// Documentation: https://developer.twitter.com/en/docs/basics/authentication/oauth-2-0
+// Expected format: "Authorization: Bearer XXXXXXXXXXXXXXXXXXXXX"
+$twitter_api_auth = NULL;
+
+?>
