16ab2f640fb5742b4c2806ede1ae89afdc5b952c
Yarmo Mackenbach <yarmo@qivro.xyz>
2020-07-04 16:00:36 +0200

    Update QR util, add to index

--- views/util/qr.php
+++ views/util/qr.php
@@ -3,6 +3,7 @@
 <h1>QR Code</h1>
 <div class="content">
     <form id="form-util-qr" method="post">
+        <p>This tool generates a QR code containing the fingerprint of your public key (<a href="https://github.com/open-keychain/open-keychain/wiki/QR-Codes">format</a>). This QR code can be scanned by apps like <a href="https://www.openkeychain.org/">OpenKeyChain</a>.</p>
         <h3>Fingerprint</h3>
         <input type="text" name="input" id="input" placeholder="Fingerprint" value="<?=$this->escape($input)?>">
         <div id="qrcode"></div>
