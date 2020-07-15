6f4d7ada410bcdc5f2eb67eb002000d2be34eeda
Yarmo Mackenbach <yarmo@qivro.xyz>
2020-07-15 14:03:50 +0200

    Add new logo

--- views/template.base.php
+++ views/template.base.php
@@ -11,7 +11,7 @@
     <body>
         <header>
             <div class="container">
-                <a href="/">Keyoxide</a>
+                <a href="/" class="logo"><img src="/assets/img/logo.png" alt="Keyoxide logo"></a>
                 <div class="spacer"></div>
                 <nav>
                     <a href="/verify">verify</a>
