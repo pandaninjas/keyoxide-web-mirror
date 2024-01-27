e52d965d02621981a1fe1250e06437aef46a8198
Yarmo Mackenbach <yarmo@yarmo.eu>
2024-01-27 22:12:43 +0100

    fix: move version endpoint

--- src/routes/main.js
+++ src/routes/main.js
@@ -31,6 +31,7 @@ import express from 'express'
 import markdownImport from 'markdown-it'
 import { readFileSync } from 'fs'
 import { getMetaFromReq } from '../server/utils.js'
+import app from '../index.js'
 
 const router = express.Router()
 const md = markdownImport({ typographer: true })
@@ -77,6 +78,19 @@ router.get('/.well-known/webfinger', (req, res) => {
     }]
   }
   res.json(body)
+})
+
+router.get('/.well-known/keyoxide/version', async (req, res) => {
+  // TODO Support responding with JSON object when requested
+
+  let versionDetails = app.get('git_branch')
+    ? `+${app.get('git_branch')}`
+    : ''
+  versionDetails += (app.get('git_branch') && app.get('git_hash'))
+    ? `.${app.get('git_hash').substring(0, 10)}`
+    : ''
+
+  return res.status(200).contentType('text/plain').send(`${app.get('keyoxide_name')}/${app.get('keyoxide_version')}${versionDetails}`)
 })
 
 router.get('/users/keyoxide', (req, res) => {
